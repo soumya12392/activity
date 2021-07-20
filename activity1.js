4b7965802a0d7e51bef581ca7620e09c448dc451
soumya12392 <83289414+soumya12392@users.noreply.github.com>
2021-07-20 20:31:13 +0530

    Add files via upload
    Initialised

---
+++ activity1.js
@@ -0,0 +1,14 @@
+var score = 0;
+
+function updateScore() {
+    score = score + 1;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+function saveScore() {
+    localStorage.setItem("Score", score);
+}
+
+function nextPage() {
+    window.location = "activity_2.html";
+}
